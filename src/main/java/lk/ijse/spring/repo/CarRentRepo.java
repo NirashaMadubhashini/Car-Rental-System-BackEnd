a28854a5175db6d6ba552ad9b8f3a6f85eb15783
NirashaMadubhashini <nirashamadu0729@gmail.com>
2022-07-11 17:31:30 +0530

    update CarRentRepo.java

--- src/main/java/lk/ijse/spring/repo/CarRentRepo.java
+++
@@ -1,7 +0,0 @@
-package lk.ijse.spring.repo;
-
-import lk.ijse.spring.entity.CarRent;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CarRentRepo extends JpaRepository<CarRent,String> {
-}
