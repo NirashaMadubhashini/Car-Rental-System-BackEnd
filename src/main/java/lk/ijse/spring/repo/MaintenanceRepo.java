93d8f8cd223b1f36cf545349c798c86ee34843a7
NirashaMadubhashini <nirashamadu0729@gmail.com>
2022-07-11 17:34:04 +0530

    update Maintenance.java

--- src/main/java/lk/ijse/spring/repo/MaintenanceRepo.java
+++
@@ -1,7 +0,0 @@
-package lk.ijse.spring.repo;
-
-import lk.ijse.spring.entity.Maintenance;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MaintenanceRepo extends JpaRepository<Maintenance, String> {
-}
